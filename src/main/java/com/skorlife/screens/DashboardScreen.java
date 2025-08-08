046bd682b6366b7591623df97827d955eb2a3eb4
Farhan <farhanmaulana111@gmail.com>
2025-08-08 13:41:53 +0700

    first commit

--- src/main/java/com/skorlife/screens/DashboardScreen.java
+++ src/main/java/com/skorlife/screens/DashboardScreen.java
@@ -13,7 +13,6 @@ import sun.net.ApplicationProxy;
 import java.time.Duration;
 import java.util.Collections;
 
-//ini commanddd anehh
 public class DashboardScreen {
     AndroidDriver driver;
     //App Bar
